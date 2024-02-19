3d74bdb9205f185bda06a5f90ffd0efdd3853b5e
caue peruque <caue19052003@gmail.com>
2024-02-19 17:28:34 -0300

    feat: using variable

---
+++ ex003/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        $first_name = "Cauê";
+        $last_name = "Peruque";
+        echo "Muito prazer, $first_name $last_name";
+    ?>
+</body>
+</html>
